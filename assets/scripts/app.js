ba36b7f4131460e3b85fba30379973516a5641bf
khoa nguyen <khoa.nguyenlethanh@gmail.com>
2020-02-09 22:25:19 +0700

    update js document

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,30 @@
 var app = {
     init: function() {
+
+        const contentWidth = window.innerWidth - 240 - 50 -50;
+        const contentHeight = window.innerHeight;
+        
+        $('.scrollable').css({width: contentWidth, height: contentHeight});
+        $('.scrollable').TrackpadScrollEmulator();
+
         this.initElementEvents();
     },
     initElementEvents: function() {
-        $('.nav-side-menu .menu-content .sub-menu').click(function() {
+
+        $(window).resize(function() {
+            $('.scrollable').TrackpadScrollEmulator('recalculate');
+        });
+
+        $('.nav-side-menu .menu-content .sub-menu li a').click(function() {
+
+            $('.nav-side-menu .menu-content .sub-menu li').removeClass('active');
+            $(this).parent().addClass('active');
+
+            let url = $(this).attr('href');
+            url = url.substring(1, url.length);
+            
             $.ajax({
-                url: 'pages/framework-from-scratch/introduction.html',
+                url: url,
                 success: function(data) {
                     $('.content').html(data);
                 }
